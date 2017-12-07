209ecd6c04fecc7acbfddc7369bbdded19329f83
msotest <m.soulard@imatechnologies.fr>
2017-12-07 11:09:16 +0100

    push cleverapps

---
+++ .travis/push.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_461e95b3-431f-4270-a2ec-19ca793bdb6e.git
+
+git --verbose --force push cleverapps master
